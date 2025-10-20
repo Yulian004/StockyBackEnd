d6515fb91e8c74b541eb0b2b218a9f13e2c68a38
commit gabri <fiamma886@gmail.com>
2025-10-20 11:40:25 +0200

    Initial commit

---
+++ src/main/java/com/generation/stockybackend/StockyBackEndApplication.java
@@ -0,0 +1,15 @@
+package com.generation.stockybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockyBackEndApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(StockyBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/stockybackend/StockyBackEndApplicationTests.java
@@ -0,0 +1,15 @@
+package com.generation.stockybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockyBackEndApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
